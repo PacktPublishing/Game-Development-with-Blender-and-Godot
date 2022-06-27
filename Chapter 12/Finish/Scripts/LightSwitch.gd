922c141714c6fe036b30f53de2b5bcd7db673d7c
Kumsal Obuz <iam@denizkumsal.com>
2022-06-27 16:28:36 -0400

    first commit

---
+++ Chapter 12/Finish/Scripts/LightSwitch.gd
@@ -0,0 +1,10 @@
+tool
+extends Spatial
+
+export(bool) var is_lit = false setget switch
+
+func switch(condition):
+	is_lit = condition
+
+func _process(_delta: float) -> void:
+	$OmniLight.visible = is_lit
