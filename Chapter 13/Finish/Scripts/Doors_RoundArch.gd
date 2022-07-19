8f07214c4bf6a819bddb0204c2a5273c0efcd746
Kumsal Obuz <iam@denizkumsal.com>
2022-07-19 11:26:35 -0400

    finishes the door's conditions

--- Chapter 13/Finish/Scripts/Doors_RoundArch.gd
+++ Chapter 13/Finish/Scripts/Doors_RoundArch.gd
@@ -3,6 +3,7 @@ extends Spatial
 export(NodePath) onready var backpack = get_node(backpack) as Node
 
 onready var key_collected:bool = false
+var is_open:bool = false
 
 func _ready():
 	backpack.connect("key_collected", self, "on_key_collected")
@@ -11,9 +12,13 @@ func on_key_collected():
 	key_collected = true
 
 func _on_Area_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player" and is_open == false:
 		if key_collected:
 			$OpenDoor.play()
-			print("Open Sesame!")
+			$AnimationPlayer.play("Open")
+			is_open = true
 		else:
 			$LockFiddling.play()
+
+func load_level():
+	print("What level?")
